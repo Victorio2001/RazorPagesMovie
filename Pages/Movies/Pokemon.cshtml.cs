aa3151245e26d712c87a0f123658dcebe66f1101
Victorio Garcia <victoriogdlp@gmail.com>
2024-11-04 23:46:42 +0100

    Ajout Crud

---
+++ Pages/Movies/Pokemon.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Pages_Movies;
+
+public class ModelPokemon : PageModel
+{
+    private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
+
+    public ModelPokemon(RazorPagesMovie.Data.RazorPagesMovieContext context)
+    {
+        _context = context;
+    }
+    
+    public void OnGet()
+    {
+        
+    }
+}
